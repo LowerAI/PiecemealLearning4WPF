4360c16265199a835af6310c7adca37ea3651d91
鼠大仙 <mrerror@qq.com>
2020-07-22 00:57:55 +0800

    增加BV1zK4y1s78p【WPF】怎么做自转和公转运动

---
+++ 糖君哒/BV1zK4y1s78p/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace BV1zK4y1s78p
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var BVector = new Vector(-Earth.Width / 2, -Earth.Height / 2);
+            var p = Sun.TranslatePoint(new Point(Sun.Width / 2, Sun.Height / 2), Earth);
+            var CVector = new Vector(p.X, p.Y);
+            var AVector = BVector + CVector;
+            var NewPoint = new Point(AVector.X, AVector.Y);
+            EarthRotate.CenterX = NewPoint.X;
+            EarthRotate.CenterY = NewPoint.Y;
+        }
+    }
+}
