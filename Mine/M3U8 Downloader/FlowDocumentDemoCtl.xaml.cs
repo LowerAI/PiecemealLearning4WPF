c4b179fb4d73394801b3b044e4203ee70f99a8a0
dongshuang <mrerror@qq.com>
2021-01-18 10:38:15 +0800

    新增FlowDocument的测试代码

---
+++ Mine/M3U8 Downloader/FlowDocumentDemoCtl.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace M3U8_Downloader
+{
+    public partial class FlowDocumentDemoCtl
+    {
+        public FlowDocumentDemoCtl()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Mine/RichTextBoxTest/ViewModel/ViewModelLocator.cs
+++ Mine/RichTextBoxTest/ViewModel/ViewModelLocator.cs
@@ -12,7 +12,6 @@
   See http://www.galasoft.ch/mvvm
 */
 
-using CommonServiceLocator;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Ioc;
 //using ServiceLocation;
@@ -31,7 +30,7 @@ namespace RichTextBoxDemo.ViewModel
         /// </summary>
         public ViewModelLocator()
         {
-            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+            //ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
 
             ////if (ViewModelBase.IsInDesignModeStatic)
             ////{
@@ -44,16 +43,16 @@ namespace RichTextBoxDemo.ViewModel
             ////    SimpleIoc.Default.Register<IDataService, DataService>();
             ////}
 
-            SimpleIoc.Default.Register<MainViewModel>();
+            //SimpleIoc.Default.Register<MainViewModel>();
         }
 
-        public MainViewModel Main
-        {
-            get
-            {
-                return ServiceLocator.Current.GetInstance<MainViewModel>();
-            }
-        }
+        //public MainViewModel Main
+        //{
+        //    get
+        //    {
+        //        return ServiceLocator.Current.GetInstance<MainViewModel>();
+        //    }
+        //}
         
         public static void Cleanup()
         {
