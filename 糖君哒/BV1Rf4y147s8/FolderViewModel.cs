6cb19627aeb3b995bb4f1099a0eed3619264bdab
鼠大仙 <mrerror@qq.com>
2021-03-11 23:57:11 +0800

    新增[糖君哒]BV1Rf4y147s8【植树节特供】怎么做一个文件目录树

---
+++ 糖君哒/BV1Rf4y147s8/FolderViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace BV1Rf4y147s8
+{
+    public class FolderViewModel
+    {
+        private DirectoryInfo Folder;
+
+        public FolderViewModel(string path)
+        {
+            Folder = new DirectoryInfo(path);
+        }
+
+        public string Name => Folder.Name;
+
+        public IEnumerable<FolderViewModel> Children => Folder.GetDirectories().Select(item => new FolderViewModel(item.FullName));
+    }
+}
